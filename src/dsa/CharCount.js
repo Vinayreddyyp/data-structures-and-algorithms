a627b28a5e3ba76e039153c8fb9f8f3884aa794d
vinay <ypvinayreddy@gmail.com>
2021-04-27 11:31:18 -0700

    first commit

---
+++ src/dsa/CharCount.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const CharCount = () => {
+	const charCount = str => {
+		debugger;
+		let result = {};
+		console.log('result', result);
+		for (var char of str) {
+			char = char.toLowerCase();
+			result[char] = ++result[char] || 1;
+		}
+		return result;
+	};
+
+	charCount('hello');
+	return (
+		<div>
+			<h2>Char Count</h2>
+		</div>
+	);
+};
+
+export default CharCount;
---
+++ src/dsa/PrimeNumber.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PrimeNumber = () => {
+	return (
+		<div>
+			<h1>Prime Number</h1>
+		</div>
+	);
+};
+
+export default PrimeNumber;
