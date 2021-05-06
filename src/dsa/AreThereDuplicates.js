64e9c9031c2cfe826bbe2815ff63dd03f3311c06
vinay <ypvinayreddy@gmail.com>
2021-05-06 12:08:31 -0700

    add finding duplicates in an array

---
+++ src/dsa/AreThereDuplicates.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const AreThereDuplicates = () => {
+	const findingDuplicates = () => {};
+
+	findingDuplicates(1, 2, 3, 5, 6);
+	return (
+		<div>
+			<h2>Finding duplicates in an givien aruguments of a function</h2>
+		</div>
+	);
+};
+
+export default AreThereDuplicates;
