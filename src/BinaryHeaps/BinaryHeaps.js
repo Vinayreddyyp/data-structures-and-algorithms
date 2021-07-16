2cdbadbe1395dc4d8fbdae1e1d1a79071baf9fa8
vinay <ypvinayreddy@gmail.com>
2021-07-16 10:22:52 -0700

    add Binary Heap algorithm

--- src/BinaryHeaps/BinaryHeaps.js
+++ src/BinaryHeaps/BinaryHeaps.js
@@ -13,7 +13,6 @@ const BinaryHeaps = () => {
 			let idx = this.values.length - 1;
 			const element = this.values[idx];
 			while (idx > 0) {
-				debugger;
 				let parentIdx = Math.floor((idx - 1) / 2);
 				let parent = this.values[parentIdx];
 				if (element <= parent) break;
@@ -22,11 +21,60 @@ const BinaryHeaps = () => {
 				idx = parentIdx;
 			}
 		}
+		//[55,39,41,18,27,12,33]
+
+		dequeue() {
+			debugger;
+			let min = this.values[0];
+			let end = this.values.pop();
+			if (this.values.length > 0) {
+				this.values[0] = end;
+				this.sinkDown();
+			}
+			return min;
+		}
+
+		//[33,39,41]
+
+		sinkDown() {
+			let idx = 0;
+			let length = this.values.length;
+			let element = this.values[0];
+			while (true) {
+				let leftChildIndx = 2 * idx + 1;
+				let rightChildIndx = 2 * idx + 2;
+				let leftChild, rightChild;
+				let swap = null;
+				if (leftChildIndx < length) {
+					leftChild = this.values[leftChildIndx];
+					if (leftChild > element) {
+						swap = leftChildIndx;
+					}
+				}
+
+				if (rightChildIndx < length) {
+					rightChild = this.values[rightChildIndx];
+					if (
+						(swap === null && rightChild > element) ||
+						(swap !== null && rightChild > leftChild)
+					) {
+						swap = rightChildIndx;
+					}
+				}
+
+				if (swap === null) break;
+
+				this.values[idx] = this.values[swap];
+				this.values[swap] = element;
+				idx = swap;
+			}
+		}
 	}
 
 	let heap = new MaxBinaryHeap();
+	debugger;
 	heap.insert(55);
-	heap.insert(24);
+	heap.dequeue();
 
 	return (
 		<div>
