3df132873385e5e6bbf4017ccb612f7d7e70f5c8
vinay <ypvinayreddy@gmail.com>
2021-08-07 10:39:15 -0700

    add config to the folder

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import PrimeNumber from './dsa/PrimeNumber';
+import CharCount from './dsa/CharCount';
+import Same from './dsa/Same';
+import Anagrams from './dsa/Anagrams';
+import SumZero from './dsa/SumZero';
+import CountUniqueValues from './dsa/CountUniqueValues';
+import MaxSubArraySum from './dsa/MaxSubArraySum';
+import SameFrequency from './dsa/SameFrequency';
+import AreThereDuplicates from './dsa/AreThereDuplicates';
+import FindingAveragePair from './dsa/FindingAveragePair';
+import IsSubSequence from './dsa/IsSubSequence';
+import MinSubArraySum from './dsa/MinSubArraySum';
+import FindingFactorial from './dsa/FindingFactorial';
+import ProductOfArray from './dsa/Recursion/ProductOfArray';
+import LongestSubString from './dsa/LongestSubString';
+import FindingPowerOfArgument from './dsa/Recursion/FindingPowerOfArguments';
+import Fibonacci from './dsa/Recursion/Fibonacci';
+import ReverseString from './dsa/Recursion/ReverseString';
+import IsPalindrome from './dsa/Recursion/IsPalindrome';
+import Flatten from './dsa/Recursion/Flatten';
+import CapitalizeFirst from './dsa/Recursion/CapitalizeFirst';
+import CapitalizeWords from './dsa/Recursion/CapitalizeWords';
+import NestedEvensum from './dsa/Recursion/NestedEvensum';
+import StringifyNumber from './dsa/Recursion/StringifyNumber';
+import CollectingStrings from './dsa/Recursion/CollectingStrings';
+import LinearSearch from './dsa/searching/LInearSearch';
+import BinarySearch from './dsa/searching/BinarySearch';
+import NaiveSearch from './dsa/searching/NaiveSearch';
+import BubbleSort from './Sorting/BuddleSort';
 
+import SelectionSort from './Sorting/SelectionSort';
+import MergeSort from './Sorting/MergeSort';
+import SingleLinkedList from '../src/LinkedList/SingleLinkedList';
+import DoublyLinkedList from '../src/LinkedList/DoublyLinkedList';
+import StacksAndQueues from '../src/StacksAndQueues/StacksAndQueues';
+import BinarySearchTree from '../src/BinarySearchTree/BinarySearchTree';
+import TreeTraversal from '../src/TreeTraversal/TreeTraversal';
+import Practice from '../src/Practice/Practice';
+import BinaryHeaps from './BinaryHeaps/BinaryHeaps';
+import MinBinaryHeaps from './BinaryHeaps/MinBinaryHeaps';
+import HashTables from './HashTables/HashTables';
+import Graphs from '../src/Graphs/Graphs';
+
+import PracticeGraphs from '../src/Practice/PracticeGraphs';
+import PracticeSinglyLinkedList from '../src/Practice/PracticeSinglyLinkedList';
+import PracticeDoublyLinkedList from '../src/Practice/PracticeDoublyLinkedList';
+import PracticeFrequencyCounterPattern from '../src/Practice/PracticeFrequencyCounterPattern';
+import PracticeMultiplePointerPattern from '../src/Practice/PracticeMultiplePointerPattern';
+import PracticeBinarySearchTree from '../src/Practice/PracticeBinarySearchTree';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			<header className="App-header">
+				<p>
+					<PracticeBinarySearchTree />
+				</p>
+			</header>
+		</div>
+	);
 }
 
 export default App;
